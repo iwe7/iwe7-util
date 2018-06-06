0d5f7f18530f98f7bf2ef43931bcaec089e0c807
iwe7 <admin@meepo.com.cn>
2018-06-06 20:15:51 +0800

    save

--- src/lib/php/var/isset.ts
+++ src/lib/php/var/isset.ts
@@ -1,14 +1,6 @@
 export function isset(a: any): boolean {
-    const l = a.length;
-    let i = 0;
-    if (l === 0) {
-        throw new Error('Empty isset');
-    }
-    while (i !== l) {
-        if (a[i] === undefined || a[i] === null) {
-            return false;
-        }
-        i++;
+    if (a === undefined || a === null) { 
+        return false;
     }
     return true;
 }
